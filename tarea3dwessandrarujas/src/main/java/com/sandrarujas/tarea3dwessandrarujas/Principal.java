0063045672a6ac3d3439e662d72f691033d61dfc
Sandra Rujas <srujasarroyo@gmail.com>
2024-12-06 17:36:14 +0100

    "Clase Principal"

--- tarea3dwessandrarujas/src/main/java/com/sandrarujas/tarea3dwessandrarujas/Principal.java
+++ tarea3dwessandrarujas/src/main/java/com/sandrarujas/tarea3dwessandrarujas/Principal.java
@@ -1,38 +1,20 @@
 package com.sandrarujas.tarea3dwessandrarujas;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-
-import com.sandrarujas.tarea3dwessandrarujas.modelo.Planta;
-import com.sandrarujas.tarea3dwessandrarujas.servicios.ServiciosEjemplar;
-import com.sandrarujas.tarea3dwessandrarujas.servicios.ServiciosPlanta;
+import com.sandrarujas.tarea3dwessandrarujas.vista.ViveroFachadaInvitado;
 
 
-public class Principal implements CommandLineRunner {
+public class Principal implements CommandLineRunner{
 	
-	//La anterior capa controladora
-	@Autowired 
-	ServiciosPlanta servplant;
-	
-	
-	@Autowired 
-	ServiciosEjemplar servejemplar;
-	
-	
-	//Antiguo Main
+	 @Autowired
+	 private ViveroFachadaInvitado viveroFachadaInvitado;
+
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("INI");
+		viveroFachadaInvitado.menuInvitado();
+  }
 		
-		Planta p =new Planta();
-		servplant.validarPlanta(p);
-		
-		servplant.validarPlanta(p);
-		
-		
-		System.out.println("----------------");
-		
-		System.out.println("FIN");
 	}
 
-}
