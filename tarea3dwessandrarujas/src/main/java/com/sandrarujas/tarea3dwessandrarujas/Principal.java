85b82ca97fa7996a16bae2dbfdde5ca9d2b07244
Sandra Rujas <srujasarroyo@gmail.com>
2024-12-11 00:27:09 +0100

    "Clase Principal documentada"

--- tarea3dwessandrarujas/src/main/java/com/sandrarujas/tarea3dwessandrarujas/Principal.java
+++ tarea3dwessandrarujas/src/main/java/com/sandrarujas/tarea3dwessandrarujas/Principal.java
@@ -1,8 +1,4 @@
 package com.sandrarujas.tarea3dwessandrarujas;
-
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import com.sandrarujas.tarea3dwessandrarujas.vista.ViveroFachadaInvitado;
@@ -15,6 +11,8 @@ public class Principal implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		/*Entramos al menú del invitado y de ahí ya podemos "viajar" a los demás*/
 		viveroFachadaInvitado.menuInvitado();
   }
 		
